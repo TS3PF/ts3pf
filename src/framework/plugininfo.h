6791382401621da607ff8150286ad9820fe50493
thenilsdev <contact@ni.ls>
2019-07-25 19:52:49 +0200

    Initial commit

---
+++ src/framework/plugininfo.h
@@ -0,0 +1,9 @@
+#ifndef PLUGININFO_H
+#define PLUGININFO_H
+
+#define PLUGININFO_NAME "Teamspeak Plugin Framework"
+#define PLUGININFO_AUTHOR "TheNilsDev"
+#define PLUGININFO_DESCRIPTION "Teamspeak Plugin Framework for faster development"
+#define PLUGININFO_VERSION "1.0.0.0"
+
+#endif // !PLUGININFO_H
